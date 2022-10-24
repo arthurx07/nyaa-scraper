3c1ba25eddf95319dfc563dd8f23406892d7e8c2
kumao <arturleivarx@gmail.com>
2022-10-24 20:24:45 +0200

    + decide torrent & pass to xdg-open

--- nyaa-scraper.py
+++ nyaa-scraper.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import sys
+import subprocess
 
 DOMAIN = 'https://nyaa.si'
 URL = 'https://nyaa.si/?f=0&c=0_0&q='+''.join(sys.argv[1:])
@@ -13,7 +14,38 @@ def get_soup(url):
     return bs(requests.get(url).text, 'html.parser')
 
 
+torrents = ['']
+title_list = ['']
 for link in get_soup(URL).find_all('a'):
     file_link = link.get('href')
     if FILETYPE in file_link:
-        print(file_link)
+        torrents.append(file_link)
+
+for i in torrents[2::2]:  # return only even indices of a list
+    j = i.split('&dn=')
+    k = j[1]
+    k = k.split('&tr=')
+    title = k[0]
+    title = title.replace('%5B', '[')
+    title = title.replace('%5D', ']')
+    title = title.replace('%20', '_')
+    title = title.replace('%28', '(')
+    title = title.replace('%2F', '/')
+    title = title.replace('%29', ')')
+    title = title.replace('%21', '!')
+    title_list.append(title)
+
+for (i, item) in enumerate(title_list, start=0):
+    print(i, item)
+
+select_input = int(input('Enter torrent to download: '))
+torrent_select = torrents[select_input*2]
+
+cmd = "/usr/bin/xdg-open {}".format(torrent_select)
+p = subprocess.Popen(cmd, shell=True, stderr=subprocess.PIPE)
+
+# to do:
+# store received torrents in a list
+# from magnet url, convert filename to readable text
+# let user select which torrent
+# let user decide to download torrent or open magnet
